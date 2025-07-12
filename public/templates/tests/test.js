621353351df3116174bc3d478572689b45acd4fb
Fernando <ferna_shadow@hotmail.com>
2025-07-11 18:39:07 -0700

    Avance forma de pago

--- public/templates/tests/test.js
+++ public/templates/tests/test.js
@@ -2,6 +2,20 @@ const slider = document.querySelector('.sliderCarousel');
 const backButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 const listHTML = document.querySelector('.sliderCarousel .list');
+const labelDate = document.querySelector('.header-right span:nth-child(2)');
+const typeOfPayment = ['Dolar','Tarjeta','Transferencia','Cupon','Efectivo'];
+let index = 0;
+
+const getCurrentDate = () => {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() +1).padStart(2, '0');
+    const year = today.getFullYear();
+
+    return `${day}/${month}/${year}`;
+}
+
+labelDate.textContent = getCurrentDate();
 
 backButton.onclick = function() {
     showSlider('back');
@@ -18,12 +32,13 @@ const showSlider = (type) => {
     const items = document.querySelectorAll('.sliderCarousel .list .item');
 
     if (type === 'next') {
-        
+        index = index >= 4 ? 0 : ++index;
         listHTML.appendChild(items[0]);
         slider.classList.add('next');
     } else {
-    listHTML.prepend(items[items.length - 1]);
-    slider.classList.add('prev');
+        index = index <= 0 ? 4 : --index;
+        listHTML.prepend(items[items.length - 1]);
+        slider.classList.add('prev');
     }
 
     clearTimeout(unAcceptClick);
